2d2b9a97c76d7d3fbf54aac9e219e83141d66e36
Ryu <96819668+balikis-oyeleye@users.noreply.github.com>
2024-11-02 19:31:57 +0100

    remove top padding from main container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="bg-main pt-8">
+    <div className="bg-main">
       <Navbar />
       <main>
         <Hero />
