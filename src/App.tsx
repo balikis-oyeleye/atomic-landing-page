1f20da7c1b4c82964de9c6c1bb0d702f5d3eb59f
Ryu <96819668+balikis-oyeleye@users.noreply.github.com>
2024-11-16 19:17:49 +0100

    Add FAQs and Metrics components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Banner from "./components/banner";
+import FAQs from "./components/faqs";
 import Features from "./components/features";
 import Hero from "./components/hero";
 import HowItWorks from "./components/how-it-works";
 import Introduction from "./components/introduction";
+import Metrics from "./components/metrics";
 import Navbar from "./components/navbar";
 import Testimonials from "./components/testimonials";
 
@@ -17,6 +19,8 @@ function App() {
         <Features />
         <HowItWorks />
         <Testimonials />
+        <Metrics />
+        <FAQs />
       </main>
     </div>
   );
